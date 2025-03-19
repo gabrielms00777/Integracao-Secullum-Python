56df8081dd0180200b49040ce0074266a126ed5b
Gabriel Moreira <gabrielms00777@gmail.com>
2025-03-19 16:38:26 -0300

    wip

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 API_AUTH_BASE_URL = "https://autenticador.secullum.com.br"
 API_BASE_URL = "https://pontowebintegracaoexterna.secullum.com.br/IntegracaoExterna"
-USERNAME = ""
-PASSWORD = ""
+USERNAME = "manutencao@pontosocorro.com.br"
+PASSWORD = "gabriel.43690"
